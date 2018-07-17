f28e62f4eb19da09f07f903fcaee3603b4b4fcc3
Drew Grubb <drew.grubb0@gmail.com>
2018-07-16 20:56:54 -0500

    Updated Jenkins Cleanup

--- maps_handler.py
+++ maps_handler.py
@@ -2,8 +2,13 @@ import requests
 import os
 import json
 
+import boto3
+import elasticsearch
+
 maps_link = "https://maps.googleapis.com/maps/api/place/textsearch/json"
 api_key = os.environ['api-key']
+
+dynamoDB = boto3.resource('dynamodb')
 
 def retrieveInformation():
     return requests.get(maps_link, api_key)
