1d7c93035ae9baf7496653e162ab04cd3220dce1
Drew <drew.grubb0@gmail.com>
2018-07-23 20:53:29 -0500

    Added file layouts and groundwork for Docker

--- init/setup.sh
+++ init/setup.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+chmod +x ./test/test.sh
+chmod +x ./test/test-remote.sh
+
 serverless create --template aws-python3
 git init
 virtualenv venv --python=python3
--- test/test-remote.sh
+++ test/test-remote.sh
@@ -1,19 +1,19 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT="$(cd $DIR && cd .. && pwd)"
 
 cd $ROOT
 
-FUNCTION=$0
+FUNCTION=$1
 
-if[[ -z $FUNCTION ]];
+if [ -z "$1" ]
 then
     echo `date`" - Usage: ./test.sh [Function Path] (Input Data Path)"
     exit 1
 fi
 
 DEFAULT_INPUT_DATA="${DIR}/empty-data.json"
-INPUT_DATA=${1:-$DEFAULT_INPUT_DATA}
+INPUT_DATA=${2:-$DEFAULT_INPUT_DATA}
 
-serverless invoke --function ${FUNCTION_PATH} --path ${INPUT_DATA}
+serverless invoke --function ${FUNCTION} --data INPUT_DATA
