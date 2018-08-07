c11098dbecb78d31efa5940ee5936a995476af27
Drew <drew.grubb0@gmail.com>
2018-08-06 19:37:49 -0500

    Added Semantic Versioning

--- scripts/get-release-from-git-and-increment.sh
+++ scripts/get-release-from-git-and-increment.sh
@@ -1,2 +1,69 @@
 #!/bin/bash
+#
+# Retrieves current release tag from github and increments it using SemVer before
+# storing the new value in the environment variable NEW_TAG.
+# Tags are released and incremented in a 0.0.0 fashion.
+#
+# Parameters: VERSION_INCREMENT ["none", "patch", "minor", "major"] : Default patch
 
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+if [ "${VERSION_INCREMENT}" == "none" ]; then
+    echo "Not releasing a new tag. Exiting..."
+    exit 1
+fi
+
+CURRENT_COMMIT=$(git rev-parse HEAD)
+echo "Acting on commit ${CURRENT_COMMIT}"
+
+CURRENT_TAG=$(git describe $(git rev-list --tags --max-count=1) --tags)
+
+if [[ $? -ne 0 ]]; then
+    echo "No tag exists. Using default tag 0.0.1"
+    NEW_TAG="0.0.1"
+else
+    CURRENT_TAG_COMMIT=$(git rev-list -n 1 ${CURRENT_TAG})
+    echo "Current tag ${CURRENT_TAG} on commit ${CURRENT_TAG_COMMIT}"
+fi
+
+if [[ "${CURRENT_COMMIT}" == "${CURRENT_TAG_COMMIT}" ]]; then
+    echo "No changes to be pushed. Exiting..."
+    exit 1
+fi
+
+git dif-index --quiet HEAD --
+if [ $? -ne 0 ]; then
+    echo "There are uncommitted changes on the current branch. Exiting..."
+    exit 1
+fi
+
+if [ -z "${VERSION_INCREMENT}" ];then
+    echo "No version increment set. incrementing by \"patch\""
+    VERSION_INCREMENT="patch"
+fi
+
+mkdir -p "${ROOT_DIR}/tmp/tagging"
+
+echo "Downloading semver..."
+SEMVER_FILE="${ROOT_DIR}/tmp/semver"
+if [ -f "${SEMVER_FILE}" ]; then
+    curl -o "${SEMVER_FILE}" https://raw.githubusercontent.com/fsaintjacques/semver-tool/2.0.0/src/semver
+    chmod +x "${SEMVER_FILE}"
+fi
+
+if [ -f "${SEMVER_FILE}" || $? -ne 0 ]; then
+    echo "Error downloading SemVer. Exiting..."
+    exit 1
+fi
+
+if [ "${NEW_TAG}" -ne "0.0.1" ]; then
+    NEW_TAG=$("${SEMVER_FILE}" bump ${VERSION_INCREMENT} ${CURRENT_TAG})
+fi
+
+echo "New tag is ${NEW_TAG}"
+echo "${NEW_TAG}" > "${ROOT_DIR}/version.txt"
+
+git tag ${NEW_TAG}
+echo "Tag successfully created."
+
+rm -rf "${ROOT_DIR}/tmp/tagging"
--- scripts/push-release-to-git.sh
+++ scripts/push-release-to-git.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
-git describe --always --tags
+NEW_TAG=${NEW_TAG}
+
+TAG=$(git describe --always --tags)
+echo "Current tag: ${TAG}"
+
+if [ -z ${NEW_TAG} ]; then
+    echo "No new tag has been created. Exiting..."
+    exit 1
+fi
+
+if [ "${TAG}" == "${NEW_TAG}" ]; then
+    echo "Tag is not being incremented. Exiting..."
+    exit 1
+fi
+
 git config --global push.default simple
 git push --tags
+
+if [[ $? -ne 0 ]]; then
+    echo "Unable to push new tag, removing tag then exiting"
+    git tag -d ${NEW_TAG}
+    git push --tags
+    exit 1
+fi
